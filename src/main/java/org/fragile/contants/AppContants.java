e1799fc6b716b322c0d525d9faafa2e54222b00a
mechack2022 <dtoms624@gmail.com>
2023-08-19 17:14:50 +0100

    app constants added

---
+++ src/main/java/org/fragile/contants/AppContants.java
@@ -0,0 +1,8 @@
+package org.fragile.contants;
+
+public class AppContants {
+
+    public static final String SECRET_KEY = "G1YT5AES305AOQSY196GF59JC54M8OLDG0P3HW3HKAQWVGAI7E";
+    public static  final String VERIFICATION_FAIL = "verification fails";
+    public static final String VERIFICATION_PASS = "verification pass";
+}
